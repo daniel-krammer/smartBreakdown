1c4c922b26c3498952abf0688e5db42ff5696b20
Daniel Krammer <daniel.krammer@mail.de>
2025-07-30 01:52:46 +0200

    Removed environments for this demo project. Removed RoutesModule and using the new module free structure.

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,13 +1,7 @@
-import { enableProdMode } from '@angular/core';
-import { environment } from './environments/environment';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { SmartBreakdownAppComponent } from './app/smart-breakdown-app.component';
 import { provideRouter } from '@angular/router';
-import { routes } from './app/smart-breakdown-app-routing.module';
-
-if (environment.production) {
-  enableProdMode();
-}
+import { routes } from './app/smart-breakdown-app.routes';
 
 bootstrapApplication(SmartBreakdownAppComponent, {
   providers: [
