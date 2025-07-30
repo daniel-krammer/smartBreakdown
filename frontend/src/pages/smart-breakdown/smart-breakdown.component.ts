4e5c0dd217dc9ed153a267955cfdc66521625eea
Daniel Krammer <daniel.krammer@mail.de>
2025-07-30 02:29:04 +0200

    Added first dumb card component

--- frontend/src/pages/smart-breakdown/smart-breakdown.component.ts
+++ frontend/src/pages/smart-breakdown/smart-breakdown.component.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from "@angular/common";
 import { ChangeDetectionStrategy, Component } from "@angular/core";
+import { CardComponent } from "src/components/card/card.component";
 
 @Component({
     selector: 'app-smart-breakdown',
     standalone: true,
-    imports: [CommonModule],
+    imports: [CommonModule, CardComponent],
     templateUrl: './smart-breakdown.component.html',
     styleUrls: ['./smart-breakdown.component.scss'],
     changeDetection: ChangeDetectionStrategy.OnPush
