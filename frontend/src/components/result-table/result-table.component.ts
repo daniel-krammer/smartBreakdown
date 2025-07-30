a4959e465786f0a7389a9097203eb58a42beb769
Daniel Krammer <daniel.krammer@mail.de>
2025-07-30 16:08:13 +0200

    Added a result table component. Adjusted the smart-breakdown page to use the table and the service.

---
+++ frontend/src/components/result-table/result-table.component.ts
@@ -0,0 +1,18 @@
+import { Input } from "@angular/core";
+
+import { CommonModule } from "@angular/common";
+import { ChangeDetectionStrategy, Component } from "@angular/core";
+import { BreakdownItem } from "src/models/breakdown.model";
+
+@Component({
+    selector: 'app-result-table',
+    standalone: true,
+    imports: [CommonModule],
+    templateUrl: './result-table.component.html',
+    styleUrls: ['./result-table.component.scss'],
+    changeDetection: ChangeDetectionStrategy.OnPush
+  })
+  export class ResultTableComponent {
+    @Input() title = "";
+    @Input() breakdownItems: BreakdownItem[] = [];
+  } 
--- frontend/src/pages/smart-breakdown/smart-breakdown.component.ts
+++ frontend/src/pages/smart-breakdown/smart-breakdown.component.ts
@@ -3,11 +3,14 @@ import { ChangeDetectionStrategy, Component } from "@angular/core";
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from "@angular/forms";
 import { CardComponent } from "src/components/card/card.component";
 import { EuroInputComponent } from "src/components/euro-input/euro.input.component";
+import { ResultTableComponent } from "src/components/result-table/result-table.component";
+import { BreakdownItem } from "src/models/breakdown.model";
+import { BreakdownService } from "src/services/breakdown.service";
 
 @Component({
     selector: 'app-smart-breakdown',
     standalone: true,
-    imports: [CommonModule, CardComponent, EuroInputComponent, ReactiveFormsModule],
+    imports: [CommonModule, CardComponent, EuroInputComponent, ReactiveFormsModule, ResultTableComponent],
     templateUrl: './smart-breakdown.component.html',
     styleUrls: ['./smart-breakdown.component.scss'],
     changeDetection: ChangeDetectionStrategy.OnPush
@@ -15,9 +18,23 @@ import { EuroInputComponent } from "src/components/euro-input/euro.input.compone
 export class SmartBreakdownComponent {
     formGroup = new FormGroup({
         euroAmount: new FormControl(0, [Validators.required])
-      });
+    });
+    breakdownItems: BreakdownItem[] = [];
+
+    constructor(private breakdownService: BreakdownService) { }
 
     onSubmit($event: Event) {
-        console.log("submitting...");
+        let euroAmount = this.formGroup.value.euroAmount;
+        if (euroAmount) {
+            if (isNaN(euroAmount)) {
+                // format euroAmount to number
+                // TODO: the euroInputComponent should handle this
+                const formattedEuroAmount = euroAmount.toString().replace(',', '.');
+                euroAmount = Number(formattedEuroAmount);
+            }
+            this.breakdownService.calculateBreakdown(euroAmount).subscribe((breakdownItems) => {
+                this.breakdownItems = breakdownItems;
+            });
+        }
     }
 }
